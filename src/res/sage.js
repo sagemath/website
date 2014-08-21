f99d045b19c45f3667dbfdee2bdaa3939fb511fe
Harald Schilly <harald.schilly@univie.ac.at>
2014-08-21 16:08:20 +0200

    * legal.html transferred * sage.js got a namespace * random links on index.html work and have a fade-in

--- src/res/sage.js
+++ src/res/sage.js
@@ -2,39 +2,43 @@
  *  author: harald schilly <harald.schilly@gmail.com>
  *  license: apache 2.0
  * */
-function tracklinks(id, cat) {
-  var list = document.getElementById(id);
-  if (!list) { return; }
-  function getTarget(x){
-    x = x || window.event;
-    return x.target || x.srcElement;
-  }
-  function klick(e) {
-    var t = getTarget(e);
-    //alert (t);
-    if (t.nodeName.toLowerCase() == 'a') {
-      var u = t.href;
-      pageTracker._trackEvent('Clicks', cat, u, 0);
-    }
-  }
-  list.addEventListener('click',klick,false);
-}
 
+var isTouch = !!('ontouchstart' in window);
 var isMac = navigator.platform.toUpperCase().indexOf('MAC')!==-1;
 var isWindows = navigator.platform.toUpperCase().indexOf('WIN')!==-1;
 var isLinux = navigator.platform.toUpperCase().indexOf('LINUX')!==-1;
 
-function downloadUrl() {
-    if (isWindows) return "download-windows.html";
-    if (isMac)     return "download-mac.html";
-    if (isLinux)   return "download-linux.html";
-    return "download.html";
-}
+var sage = {
+    tracklinks: function (id, cat) {
+        var list = document.getElementById(id);
+        if (!list) { return; }
+        function getTarget(x){
+            x = x || window.event;
+            return x.target || x.srcElement;
+        }
+        function klick(e) {
+            var t = getTarget(e);
+            //alert (t);
+            if (t.nodeName.toLowerCase() == 'a') {
+              var u = t.href;
+              pageTracker._trackEvent('Clicks', cat, u, 0);
+            }
+        }
+        list.addEventListener('click',klick,false);
+    },
 
-function setDownloadUrls() {
-    $("a[downloadurl]").each(function() {
-        this.href = downloadUrl();
-    });
-}
+    downloadUrl: function() {
+        if (isWindows) return "download-windows.html";
+        if (isMac)     return "download-mac.html";
+        if (isLinux)   return "download-linux.html";
+        return "download.html";
+    },
 
-$(setDownloadUrls);
+    setDownloadUrls: function () {
+        $("a[downloadurl]").each(function() {
+            this.href = sage.downloadUrl();
+        });
+    }
+};
+
+$(sage.setDownloadUrls);
