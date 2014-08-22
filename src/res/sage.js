51ac14c77719190f1f3b045c3604129995284b65
Harald Schilly <harald.schilly@gmail.com>
2014-08-22 16:06:20 +0200

    navbar: sticky on top when scrolling

--- src/res/sage.js
+++ src/res/sage.js
@@ -38,7 +38,32 @@ var sage = {
         $("a[downloadurl]").each(function() {
             this.href = sage.downloadUrl();
         });
+    },
+
+    scrollNavbarInit: function () {
+        var $w = $(window);
+        var $navbar = $('#sage-nav');
+        var navbar_top = $navbar.offset().top;
+        $navbar.css({'width' : $navbar.width()});
+        var height = $navbar.height();
+
+        /* note to self: animation is cute, but one has to device a way to re-trigger it while animations are still running */
+        var scrollNavbar = function(){
+            var curtop = $w.scrollTop();
+            if (curtop > navbar_top) {
+                $navbar.css({"position" : "fixed", "top" : 0, 'height' : "3em", background: "#e9e9f9"});
+                /* $navbar.animate({'height' : "3em", background: "#e9e9f9"}); */
+            } else {
+                $navbar.css({'position': 'relative', "height" : height, background: "#fff"});
+                /* $navbar.animate({"height" : height, background: "#fff" }); */
+            }
+        };
+
+        /* run it once and then every time the window scrolls */
+        scrollNavbar();
+        $(window).scroll(scrollNavbar);
     }
 };
 
 $(sage.setDownloadUrls);
+$(sage.scrollNavbarInit);
